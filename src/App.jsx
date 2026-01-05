9747d2bcc06f5909e877e548f05d154600bc7f67
venkatesh.singarapu <venkatesh.singarapu@fusion5tech.in>
2026-01-05 16:45:38 +0530

    add p

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,11 @@ function App() {
     <h1>Welcome to ERP Project</h1>
     
     <h2>start the project</h2>
+    <h3>web development</h3>
     <h4>frontend project</h4>
     <h5>developer</h5>
      <p>ERP multi store</p>
+     <p>dashboard</p>
      
     </>
   )
