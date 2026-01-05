d68cfbb081dc4b67a401b506946420f9a69afed1
vikram <vikramsingh.patimimn@fusion5tech.in>
2026-01-05 14:24:33 +0530

    adding h6

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
     <h1>Welcome to ERP Project</h1>
@@ -14,7 +11,7 @@ function App() {
     <h4>frontend project</h4>
     <h5>developer</h5>
      <p>ERP multi store</p>
-     
+     <h6>ERP omnistore</h6>
     </>
   )
 }
