c339fc1617a16ccf37039356dbc1c4932a5eae6e
bhaskar <bhaskar.pitta@fusion5tech.in>
2026-01-05 13:05:54 -0600

    remove h2

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,7 @@ function App() {
   return (
     <>
     <h1>Welcome to ERP Project</h1>
-    
-    <h2>start the project</h2>
-    <h4>frontend project</h4>
-    <h5>developer</h5>
-     <p>ERP multi store</p>
-     <h6>ERP omnistore</h6>
-     <h3>ERP STORE</h3>
-     </>
+    </>
   )
 }
 
